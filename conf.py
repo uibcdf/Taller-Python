6ab036482612bb30f800cbde7ea8a2eaac4b9638
Diego Prada-Gracia <prada.gracia@gmail.com>
2023-05-10 00:57:06 -0600

    Primer video en ediciones

--- conf.py
+++ conf.py
@@ -104,7 +104,11 @@ pygments_style = 'default'
 html_theme = 'sphinx_book_theme'
 #html_theme = 'pydata_sphinx_theme'
 html_theme_options = {
-            "logo": {"alt_text": "UIBCDF-Talleres"},
+            "logo": {"alt_text": "UIBCDF Taller Python"},
+            "use_source_button": True,
+            "use_repository_button": True,
+            "repository_provider": "github",
+            "repository_url": "https://github.com/uibcdf/Taller-Python"
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
